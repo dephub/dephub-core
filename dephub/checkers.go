c16901c3fa0b6815eea17b4283cc622b090351fa
godstanis <godstanis@gmail.com>
2021-01-30 16:53:36 +0300

    Add Composer packages update checker implementation

--- dephub/checkers.go
+++ dephub/checkers.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/dephub/dephub-core/providers/api/packagist"
 	"github.com/dephub/dephub-core/providers/api/pip"
 	"github.com/dephub/dephub-core/providers/versioneer"
 )
@@ -102,4 +104,186 @@ skip_pkg:
 	}
 
 	return result, nil
+}
+
+// NewComposerUpdatesChecker constructs new ComposerUpdatesChecker.
+func NewComposerUpdatesChecker(httpClient *http.Client) UpdatesChecker {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	api, err := packagist.NewClient(httpClient, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &ComposerUpdatesChecker{api: api}
+}
+
+// ComposerUpdatesChecker represents Composer packages update checker.
+type ComposerUpdatesChecker struct {
+	api packagist.Client
+}
+
+// CompatibleUpdates returns latest available updates for locked dependencies compatible with constraints.
+//
+// Basically it is 'your locked dependency is lower then available with your constraints'
+func (uc ComposerUpdatesChecker) CompatibleUpdates(ctx context.Context, constraints []Constraint, requirements []Requirement) ([]Update, error) {
+	if len(requirements) == 0 || len(constraints) == 0 {
+		return nil, fmt.Errorf("no packages provided")
+	}
+
+	// To optimize requirements filtering
+	reqsLookup := make(map[string]*Requirement)
+	for i, req := range requirements {
+		reqsLookup[req.Name] = &requirements[i]
+	}
+
+	result := make([]Update, 0, len(constraints))
+
+	for _, cns := range constraints {
+		if _, ok := reqsLookup[cns.Name]; !ok {
+			continue
+		}
+		req := reqsLookup[cns.Name]
+
+		metaData, err := uc.getPackagistMeta(ctx, uc.api, cns.Name)
+		if err != nil {
+			continue
+		}
+
+		update, err := uc.compatibleReleases(cns, *req, true, true, metaData)
+		if err != nil {
+			continue
+		}
+
+		if len(update) != 0 {
+			result = append(result, *update[0])
+		}
+	}
+
+	return result, nil
+}
+
+// compatibleReleases returns list of updates available for package
+// updatable - show only constraint satisfying next versions
+func (uc ComposerUpdatesChecker) compatibleReleases(constraint Constraint, req Requirement, updatable bool, first bool, meta packagist.PackageMeta) ([]*Update, error) {
+	if len(meta) == 0 {
+		return nil, fmt.Errorf("meta info is empty")
+	}
+
+	baseCst, err := versioneer.NewComposerConstraints(constraint.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	reqCst, err := versioneer.NewComposerConstraints(">" + req.Version)
+	if err != nil {
+		return nil, err
+	}
+
+	releases := make([]*Update, 0, 5)
+
+	// Filter parsable versions
+	for i := len(meta) - 1; i >= 0; i-- {
+		vers, err := versioneer.NewComposerVersion(meta[i].Version)
+		if err != nil {
+			continue
+		}
+
+		include := reqCst.Match(vers)
+		if updatable {
+			include = include && baseCst.Match(vers)
+		}
+		if include {
+			update := composerVersionToUpdate(meta[i])
+			update.CurrentVersion = req.Version
+			update.CurrentConstraint = constraint.Version
+			releases = append(releases, update)
+			if first {
+				return releases, nil
+			}
+		}
+	}
+
+	return releases, nil
+}
+
+// LastUpdates returns latest versions for each package
+func (uc ComposerUpdatesChecker) LastUpdates(ctx context.Context, packages []Constraint, incompatibleOnly bool) ([]Update, error) {
+	if len(packages) == 0 {
+		return nil, fmt.Errorf("no packages provided")
+	}
+
+	result := make([]Update, 0, len(packages))
+
+skip_pkg:
+	for _, pkg := range packages {
+		metaData, err := uc.getPackagistMeta(ctx, uc.api, pkg.Name)
+		if err != nil {
+			continue
+		}
+
+		constraint, err := versioneer.NewComposerConstraints(pkg.Version)
+		if err != nil {
+			continue
+		}
+
+		var update *Update
+		// Filter first (from the newest) parsable version
+		for i := len(metaData) - 1; i >= 0; i-- {
+			vers, err := versioneer.NewComposerVersion(metaData[i].Version)
+			if err != nil {
+				continue
+			}
+
+			// If we only need incompatible versions and the last version matches the constraint
+			// then skip the package, it is already up do date
+			if incompatibleOnly && constraint.Match(vers) {
+				continue skip_pkg
+			}
+
+			update = composerVersionToUpdate(metaData[i])
+			break
+		}
+
+		if update != nil {
+			update.CurrentConstraint = pkg.Version
+			result = append(result, *update)
+		}
+	}
+
+	return result, nil
+}
+
+// getPackagistMeta returns meta information about the package from packagist api.
+func (uc ComposerUpdatesChecker) getPackagistMeta(ctx context.Context, cl packagist.Client, pkg string) (packagist.PackageMeta, error) {
+	pkgNamePrts := strings.Split(pkg, "/")
+	if len(pkgNamePrts) != 2 {
+		return nil, fmt.Errorf("cannot parse vendor from package name %q", pkg)
+	}
+	vendor, name := pkgNamePrts[0], pkgNamePrts[1]
+	metaData, _, err := cl.Meta(ctx, vendor, name)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := metaData.Packages[pkg]; !ok {
+		return nil, fmt.Errorf("package %q not found on packagist", pkg)
+	}
+	return metaData.Packages[pkg], err
+}
+
+// composerVersionToUpdate is a little helper to convert VersionMeta to Update type.
+func composerVersionToUpdate(release packagist.VersionMeta) *Update {
+	update := &Update{
+		Name:    release.Name,
+		URL:     release.Source.URL,
+		Version: release.Version,
+		Author:  release.Name,
+	}
+
+	if len(release.Authors) != 0 {
+		update.Author = release.Authors[0].Name
+	}
+	return update
 }
--- dephub/checkers_test.go
+++ dephub/checkers_test.go
@@ -5,10 +5,31 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/dephub/dephub-core/providers/api/packagist"
 	"github.com/dephub/dephub-core/providers/api/pip"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+type PackagistMock struct {
+	mock.Mock
+	packagist.PackagistClient
+}
+
+func (mock *PackagistMock) Meta(ctx context.Context, vendor, pkg string) (*packagist.PackagesMeta, *http.Response, error) {
+	args := mock.Called(ctx, vendor, pkg)
+	var f *packagist.PackagesMeta
+	var s *http.Response
+	// To allow nil values
+	if mt, ok := args.Get(0).(*packagist.PackagesMeta); ok {
+		f = mt
+	}
+	if resp, ok := args.Get(1).(*http.Response); ok {
+		s = resp
+	}
+
+	return f, s, args.Error(2)
+}
 
 // PyPiMock mocks PyPiClient logic.
 type PyPiMock struct {
@@ -30,6 +51,106 @@ func (mock *PyPiMock) Release(ctx context.Context, name, version string) (*pip.P
 	}
 
 	return f, s, args.Error(2)
+}
+
+func TestComposerUpdatesChecker_NewMethod(t *testing.T) {
+	cl := NewComposerUpdatesChecker(nil)
+	assert.True(t, cl.(*ComposerUpdatesChecker).api != nil)
+}
+
+func TestComposerUpdatesChecker_LastUpdatesMethod(t *testing.T) {
+	coreSource := NewMemorySource(sourceMockFileStorage)
+	// Set our mock to always return one result on every Meta call.
+	apiMock := new(PackagistMock)
+	apiMock.On("Meta", mock.Anything, mock.Anything, mock.Anything).Return(&composerPackagesMeta, nil, nil)
+
+	expectedUpdates := []Update{
+		{Name: "testing/something", Author: "testing/something", Version: "2.1.17", CurrentConstraint: "2.0.*"},
+		{Name: "another/testpackage", Author: "another/testpackage", Version: "3.7.0", CurrentConstraint: "3.5.*"},
+	}
+
+	uc := ComposerUpdatesChecker{api: apiMock}
+
+	constraints, err := coreSource.Constraints(context.Background(), ComposerType)
+	if err != nil {
+		t.Fatalf("unexpected error on source constraints: %v", err)
+	}
+
+	updates, err := uc.LastUpdates(context.Background(), constraints, true)
+	if err != nil {
+		t.Fatalf("unexpected error on last updates: %v", err)
+	}
+
+	assert.Len(t, updates, 2)
+	assert.ElementsMatch(t, expectedUpdates, updates)
+	apiMock.AssertExpectations(t)
+}
+
+func TestComposerUpdatesChecker_LastUpdatesMethod_WithCompatible(t *testing.T) {
+	coreSource := NewMemorySource(sourceMockFileStorage)
+	// Set our mock to always return one result on every Meta call.
+	apiMock := new(PackagistMock)
+	apiMock.On("Meta", mock.Anything, mock.Anything, mock.Anything).Return(&composerPackagesMeta, nil, nil)
+
+	expectedUpdates := []Update{
+		{Name: "testing/something", Author: "testing/something", Version: "2.1.17", CurrentConstraint: "2.0.*"},
+		{Name: "another/testpackage", Author: "another/testpackage", Version: "3.7.0", CurrentConstraint: "3.5.*"},
+		{Name: "test/package", Author: "test/package", Version: "1.2.3", CurrentConstraint: ">=1.0.0"},
+	}
+
+	uc := ComposerUpdatesChecker{api: apiMock}
+
+	constraints, err := coreSource.Constraints(context.Background(), ComposerType)
+	if err != nil {
+		t.Fatalf("unexpected error on source constraints: %v", err)
+	}
+
+	updates, err := uc.LastUpdates(context.Background(), constraints, false)
+	if err != nil {
+		t.Fatalf("unexpected error on last updates: %v", err)
+	}
+
+	assert.Len(t, updates, 3)
+	assert.ElementsMatch(t, expectedUpdates, updates)
+	apiMock.AssertExpectations(t)
+}
+
+func TestComposerUpdatesChecker_CompatibleUpdatesMethod(t *testing.T) {
+	coreSource := NewMemorySource(sourceMockFileStorage)
+	// Set our mock to always return one result on every Meta call.
+	apiMock := new(PackagistMock)
+	apiMock.On("Meta", mock.Anything, mock.Anything, mock.Anything).Return(&composerPackagesMeta, nil, nil)
+
+	expectedUpdates := []Update{
+		{Name: "another/testpackage", Author: "another/testpackage", Version: "3.5.19", CurrentVersion: "v3.5.2", CurrentConstraint: "3.5.*"},
+		{Name: "testing/something", Author: "testing/something", Version: "2.0.3", CurrentVersion: "v1.9.17", CurrentConstraint: "2.0.*"},
+	}
+
+	uc := ComposerUpdatesChecker{api: apiMock}
+
+	constraints, err := coreSource.Constraints(context.Background(), ComposerType)
+	if err != nil {
+		t.Fatalf("unexpected error on source constraints: %v", err)
+	}
+	reqs, err := coreSource.Requirements(context.Background(), ComposerType)
+	if err != nil {
+		t.Fatalf("unexpected error on source constraints: %v", err)
+	}
+
+	updates, err := uc.CompatibleUpdates(context.Background(), []Constraint{}, []Requirement{})
+	if err == nil || err.Error() != "no packages provided" {
+		t.Error("expected error on empty packages, got none")
+	}
+	assert.Len(t, updates, 0)
+
+	updates, err = uc.CompatibleUpdates(context.Background(), constraints, reqs)
+	if err != nil {
+		t.Error("expected no errors, got: %w", err)
+	}
+
+	assert.Len(t, updates, 2)
+	assert.ElementsMatch(t, expectedUpdates, updates)
+	apiMock.AssertExpectations(t)
 }
 
 func TestPIPUpdatesChecker_NewMethod(t *testing.T) {
@@ -58,7 +179,7 @@ func TestPIPUpdatesChecker_LastUpdatesMethod(t *testing.T) {
 		t.Fatalf("unexpected error on source constraints: %v", err)
 	}
 
-	updates, err := uc.LastUpdates(context.TODO(), constraints, true)
+	updates, err := uc.LastUpdates(context.Background(), constraints, true)
 	if err != nil {
 		t.Fatalf("unexpected error on last updates: %v", err)
 	}
@@ -90,7 +211,7 @@ func TestPIPUpdatesChecker_LastUpdatesMethod_WithCompatible(t *testing.T) {
 		t.Fatalf("unexpected error on source constraints: %v", err)
 	}
 
-	updates, err := uc.LastUpdates(context.TODO(), constraints, false)
+	updates, err := uc.LastUpdates(context.Background(), constraints, false)
 	if err != nil {
 		t.Fatalf("unexpected error on last updates: %v", err)
 	}
@@ -107,13 +228,13 @@ func TestPIPUpdatesChecker_CompatibleUpdatesMethod(t *testing.T) {
 
 	uc := PIPUpdatesChecker{api: apiMock}
 
-	updates, err := uc.CompatibleUpdates(context.TODO(), []Constraint{}, []Requirement{})
+	updates, err := uc.CompatibleUpdates(context.Background(), []Constraint{}, []Requirement{})
 	if err == nil || err.Error() != "no packages provided" {
 		t.Error("expected error on empty packages, got none")
 	}
 	assert.Len(t, updates, 0)
 
-	updates, err = uc.CompatibleUpdates(context.TODO(), []Constraint{{}}, []Requirement{{}})
+	updates, err = uc.CompatibleUpdates(context.Background(), []Constraint{{}}, []Requirement{{}})
 	if err != nil {
 		t.Error("expected no errors, got: %w", err)
 	}
@@ -121,6 +242,25 @@ func TestPIPUpdatesChecker_CompatibleUpdatesMethod(t *testing.T) {
 	assert.Len(t, updates, 0)
 	apiMock.AssertExpectations(t)
 }
+
+var composerPackagesMeta = packagist.PackagesMeta{Packages: map[string]packagist.PackageMeta{
+	"test/package": {
+		{Version: "0.8.19", Name: "test/package"},
+		{Version: "1.1.7", Name: "test/package"},
+		{Version: "1.2.3", Name: "test/package"},
+	},
+	"another/testpackage": {
+		{Version: "3.2.5", Name: "another/testpackage"},
+		{Version: "3.5.19", Name: "another/testpackage"},
+		{Version: "3.7.0", Name: "another/testpackage"},
+	},
+	"testing/something": {
+		{Version: "1.2.5", Name: "testing/something"},
+		{Version: "1.5.19", Name: "testing/something"},
+		{Version: "2.0.3", Name: "testing/something"},
+		{Version: "2.1.17", Name: "testing/something"},
+	},
+}}
 
 var pipReleases = map[string]*pip.PipPackage{
 	"MyPackage": {
